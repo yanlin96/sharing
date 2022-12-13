f7fa8bfc89adda0129f61b9d79875318a6442014
NielWindows <NielWindows>
2022-12-14 00:06:33 +1100

    home format  change

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,22 +1,46 @@
-import './index.scss';
-import Loader from 'react-loaders';
-import AnimatedLetters from '../AnimatedLetters';
+import "./index.scss";
+import Loader from "react-loaders";
+import AnimatedLetters from "../AnimatedLetters";
+import pic from "../../assets/images/img.jpeg";
+
+import Grid from "@mui/material/Grid";
 
 const Home = () => {
-    return(
-        <div className='home-page'>
-            <h1>
-                <AnimatedLetters strArray={"Jenny's Portfolio".split('')} startIdx={1}/>
-                <br />
-                <AnimatedLetters strArray={"Front End Dev".split('')} startIdx={18}/>
+  return (
+    <div className="home-page">
+      <div className="container">
+        <Grid container justifyContent="center">
+          <Grid item xs={12} md={4}>
+            <h1 style={{ textAlign: "center" }}>
+              <AnimatedLetters
+                strArray={"Jenny's Portfolio".split("")}
+                startIdx={1}
+              />
+              <br />
+              <AnimatedLetters
+                strArray={"Front End Dev".split("")}
+                startIdx={18}
+              />
             </h1>
-            <Loader type="line-scale" color="#fed002" width={500} />
-            <div className='img-container'>
-                <img src="https://www.linkedin.com/in/ju-jenny-sun?miniProfileUrn=urn%3Ali%3Afs_miniProfile%3AACoAAD8kqRcBlWiM1A61iLcODxg_qqzsYByqg1w&lipi=urn%3Ali%3Apage%3Ad_flagship3_search_srp_all%3Bg%2BhbmzqEREaYXG805m7%2BUA%3D%3D" alt="image"/>
+            <p style={{ textAlign: "center", fontSize: "20px" }}>
+              {" "}
+              <AnimatedLetters
+                strArray={"A new journey to digital world".split("")}
+                startIdx={1}
+              />
+            </p>
+          </Grid>
+          <Grid item xs={12} md={8} justifyContent="center">
+            <div className="img-container">
+              <img className="img" src={pic} alt="jenny" />
             </div>
-        </div>
-       
-    );
-}
+          </Grid>
+        </Grid>
+      </div>
+
+      <Loader type="line-scale" color="#fed002" width={500} />
+    </div>
+  );
+};
 
 export default Home;
